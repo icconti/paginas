2265bd44c7f34e6d4ac6715ead4d62f6f1913e0e
Martha Cordero <96263585+icconti@users.noreply.github.com>
2022-02-12 17:15:48 -0500

    Pronóstico.cs

---
+++ Pronóstico.cs
@@ -0,0 +1,12 @@
+espacio de nombres  a__Ejemplo_1 . datos ;
+
+ clase  pública WeatherForecast
+{
+     DateTime  público Fecha { get ; conjunto ; }
+
+    public  int  TemperaturaC { get ; conjunto ; }
+
+    public  int  TemperatureF  =>  32  + ( int )( TemperatureC  /  0 . 5556 );
+
+     cadena pública ? Resumen { obtener ; conjunto ; }
+}
